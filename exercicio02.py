4fe06065a1c87b740889cdc1c32d82707ccf33fb
thomthomtt <thomazgt39@gmail.com>
2025-04-28 15:34:38 -0300

    trago atualizações

--- exercicio02.py
+++ exercicio02.py
@@ -1,4 +1,19 @@
 #fiz do jeito mais porco possivel sono duca boy
-aluno = [input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: ")]
-print(aluno)
+# aluno = [input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: ")]
+# print(aluno)
 
+###codigo do professor(segundo exercicio)
+
+# alunos=[""]*5
+# for i in range(len(alunos)):
+#     alunos[i]=input("Digite seu nome: ")
+# for x in range(len(alunos)):
+#     print(f"{alunos[x]} está na posição {x}")
+
+###codigo do terceiro exercicio
+
+nomes=["Taynara", "Raynara", "Joao", "Lopes", "Felix"]
+posicao=input("Digite um nome: ")
+for x in range(len(nomes)):
+    if posicao == nomes[x]:
+     print(f"Ele está no nosso banco de dados! Aqui esta: {nomes[x]} está na posição {x}")
