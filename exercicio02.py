17875f9441885777e5e4b543967238c5185375e7
thomthomtt <thomazgt39@gmail.com>
2025-04-28 14:34:41 -0300

    QUE SONOOOOOOOOOOOOOOOOO

---
+++ exercicio02.py
@@ -0,0 +1,4 @@
+#fiz do jeito mais porco possivel sono duca boy
+aluno = [input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: "),input("Digite seu nome: ")]
+print(aluno)
+
